60464f4c06b011c156fad2764e52b864d4da83cc
Diego Zaragoza <viene_el_gol@hotmail.com>
2021-09-23 10:50:40 -0300

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,22 @@
+
+
+
+export const getGif = async ( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=5BPCazD2xcxQsrDnUoQ6VgZZuJH3zqXr`;
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map ( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+
+    // console.log( data );
+    return gifs;
+    // setImages( gifs );
+
+}
